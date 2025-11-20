b210e3930b3f721a7fe2dc7c95c53f2d66d8323a
DAVID <76133201+majestuoso@users.noreply.github.com>
2025-11-20 17:15:13 -0300

    Corrige configuración de conexión en db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,14 @@
+// /db/index.js
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "sistema_reservas_vuelos", // <-- base correcta
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "sistema_vuelos", 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
